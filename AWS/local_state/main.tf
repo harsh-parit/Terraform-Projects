7d18f3819da8ef403c9661f2aba5027021e05596
Harsh Parit <134676833+harsh-parit@users.noreply.github.com>
2024-04-25 08:37:25 +0530

    Create main.tf

---
+++ AWS/local_state/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hasicorp/aws"
+            version = "0.5"
+        }
+    }
+}
+
+# Configure the AWS provider
+resource "aws" "Terraform-example" {
+    region = "ap-southeast-2"
+}
+
+# Create a VPC 
+resource "aws_vpc" "Terraform-example" {
+    cidr_block = "10.0.0.0/16"
+  
+}
+
+# instance configurations
+resource "aws_instance" "Terraform-example" {
+    ami = "ami- 0dcc1e21636832c5d"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = Terraform-example
+    }
+  
+}
